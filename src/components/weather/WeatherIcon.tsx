af01142c9c45518a579189d3b1b3619ddd36d917
takoonit <takoon.it@gmail.com>
2025-05-06 01:53:23 +0700

    Add WeatherIcon component to display weather icons from OpenWeatherMap API

---
+++ src/components/weather/WeatherIcon.tsx
@@ -0,0 +1,24 @@
+'use client';
+import React from 'react';
+import Image from 'next/image';
+
+type WeatherIconProps = {
+  iconCode: string;
+  alt?: string;
+  size?: number;
+};
+
+export default function WeatherIcon({ iconCode, alt = '', size = 60 }: WeatherIconProps) {
+  const src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={size}
+      height={size}
+      unoptimized // avoids warning from Next.js if image domains not configured
+      loading='eager'
+    />
+  );
+}
